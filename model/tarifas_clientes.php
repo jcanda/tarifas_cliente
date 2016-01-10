df16af17c7035e46f761fdc9d07eede79e833cf5
Francesc Pineda Segarra <shawe.ewahs@gmail.com>
2016-01-10 12:40:02 +0100

    Eliminadas comillas abiertas en consultas SQL para que funcione en PostgreSQL

--- model/tarifas_clientes.php
+++ model/tarifas_clientes.php
@@ -12,7 +12,7 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -20,12 +20,12 @@
 /**
  * Permite tener un listado con tarifas personalizadas para cada cliente de cualquier articulo
  * Por defecto siempre cojerá la tarifa mas actualizada.
- */ 
+ */
 class tarifas_clientes extends fs_model
 {
    public $tarifa_id;
    public $tarifa_fecha;
-   public $tarifa_codarticulo;   
+   public $tarifa_codarticulo;
    public $tarifa_pvp;
    public $tarifa_motivo;
    public $tarifa_codcliente;
@@ -34,15 +34,15 @@ class tarifas_clientes extends fs_model
    public function __construct( $p = FALSE)
    {
       parent::__construct('tarifas_cliente', 'plugins/tarifas_cliente/');
-      
+
       if($p)
       {
          $this->tarifa_id = $p['tarifa_id'];
-         
+
          $this->tarifa_fecha = NULL;
          if(isset($p['tarifa_fecha']) )
-            $this->tarifa_fecha = date('d-m-Y', strtotime($p['tarifa_fecha']));         
-         
+            $this->tarifa_fecha = date('d-m-Y', strtotime($p['tarifa_fecha']));
+
          $this->tarifa_codarticulo = $p['tarifa_codarticulo'];
          $this->tarifa_pvp = floatval($p['tarifa_pvp']);
          $this->tarifa_motivo = $this->no_html($p['tarifa_motivo']);
@@ -65,7 +65,7 @@ class tarifas_clientes extends fs_model
    {
       return '';
    }
-   
+
    public function exists()
    {
       if( is_null($this->tarifa_id) )
@@ -73,36 +73,36 @@ class tarifas_clientes extends fs_model
       else
          return $this->db->select("SELECT * FROM ".$this->table_name."
             WHERE tarifa_id = ".$this->var2str($this->tarifa_id).";");
-   } 
-   
+   }
+
    public function test()
    {
       $status = FALSE;
-      
+
       $this->tarifa_codcliente = trim($this->tarifa_codcliente);
       $this->tarifa_codarticulo = trim($this->tarifa_codarticulo);
       $this->tarifa_pvp = trim($this->tarifa_pvp);
       $this->tarifa_motivo = $this->no_html($this->tarifa_motivo);
 
-      
+
       if( !preg_match("/^[A-Z0-9]{1,6}$/i", $this->tarifa_codcliente) )
          $this->new_error_msg("Código de cliente no válido.");
       else if( strlen($this->tarifa_pvp) < 1 )
          $this->new_error_msg("Precio de tarifa no indicado.");
       else if( strlen($this->tarifa_codarticulo) < 1 )
-         $this->new_error_msg("Articulo de tarifa no indicado.");      
+         $this->new_error_msg("Articulo de tarifa no indicado.");
       else
          $status = TRUE;
-      
+
       return $status;
    }
-   
+
    public function save() {
-       
-        if ($this->test()) {            
+
+        if ($this->test()) {
             if ($this->exists()) return FALSE;
             else {
-                $sql = "INSERT INTO `tarifas_cliente` (`tarifa_fecha`, `tarifa_codarticulo`, `tarifa_pvp`, `tarifa_motivo`, `tarifa_codcliente`, `tarifa_agente`) 
+                $sql = "INSERT INTO tarifas_cliente (tarifa_fecha, tarifa_codarticulo, tarifa_pvp, tarifa_motivo, tarifa_codcliente, tarifa_agente) 
                VALUES (" . $this->var2str($this->tarifa_fecha) . "," . $this->var2str($this->tarifa_codarticulo) . ",
                " . $this->var2str($this->tarifa_pvp) . "," . $this->var2str($this->tarifa_motivo) . ",
                " . $this->var2str($this->tarifa_codcliente) . "," . $this->var2str($this->tarifa_agente) . ");";
@@ -115,80 +115,78 @@ class tarifas_clientes extends fs_model
             }
         } else
             return FALSE;
-    }    
-      
+    }
+
    public function all($offset=0, $limit=FS_ITEM_LIMIT) {
         $tarifas = array();
-        
-        $sql = "SELECT * FROM `tarifas_cliente` WHERE 1 ORDER BY tarifa_fecha DESC";
-        
+
+        $sql = "SELECT * FROM tarifas_cliente WHERE 1 ORDER BY tarifa_fecha DESC";
+
         $data = $this->db->select_limit($sql, $limit, $offset);
-        
+
         if ($data) {
             foreach ($data as $d)
                 $tarifas[] = new tarifas_clientes($d);
         }
 
-        return $tarifas;       
-   }    
+        return $tarifas;
+   }
 
    public function delete()
    {
-       return $this->db->exec("DELETE FROM `tarifas_cliente` WHERE tarifa_id = ".$this->var2str($this->tarifa_id).";");
+       return $this->db->exec("DELETE FROM tarifas_cliente WHERE tarifa_id = ".$this->var2str($this->tarifa_id).";");
    }
-   
+
   public function get($id)
    {
-      $sql = "SELECT * FROM `tarifas_cliente` WHERE tarifa_id = " . $this->var2str($id) . ";";
-        
+      $sql = "SELECT * FROM tarifas_cliente WHERE tarifa_id = " . $this->var2str($id) . ";";
+
       $data = $this->db->select($sql);
-      
+
       if($data)
          return new tarifas_clientes($data[0]);
       else
-         return FALSE;       
-   } 
-   
+         return FALSE;
+   }
+
    public function get_tarifas_cliente($id)
    {
       $tarifas = array();
-        
-      $sql = "SELECT * FROM `tarifas_cliente` WHERE tarifa_codcliente = " . $this->var2str($id) . " ORDER BY tarifa_codarticulo, tarifa_fecha DESC;";
-      
+
+      $sql = "SELECT * FROM tarifas_cliente WHERE tarifa_codcliente = " . $this->var2str($id) . " ORDER BY tarifa_codarticulo, tarifa_fecha DESC;";
+
       $data = $this->db->select($sql);
-      
+
       if ($data) {
           foreach ($data as $d)
               $tarifas[] = new tarifas_clientes($d);
-      }      
-      
+      }
+
       return $tarifas;
    }
    public function get_tarifas_cliente_select($id)
    {
       $tarifas = array();
 
-      $sql = "SELECT * FROM `tarifas_cliente` WHERE tarifa_id IN (SELECT max(tarifa_id) FROM `tarifas_cliente` WHERE tarifa_codcliente = " . $this->var2str($id)." GROUP BY tarifa_codarticulo ) ORDER BY tarifa_fecha DESC;";
-      
+      $sql = "SELECT * FROM tarifas_cliente WHERE tarifa_id IN (SELECT max(tarifa_id) FROM tarifas_cliente WHERE tarifa_codcliente = " . $this->var2str($id)." GROUP BY tarifa_codarticulo ) ORDER BY tarifa_fecha DESC;";
+
       $data = $this->db->select($sql);
-      
+
       if ($data) {
           foreach ($data as $d)
               $tarifas[] = new tarifas_clientes($d);
-      }      
-      
+      }
+
       return $tarifas;
    }
-   
+
    public function nombre_articulo(){
-       
+
        $date= $this->db->select("SELECT descripcion FROM articulos WHERE referencia =".$this->var2str($this->tarifa_codarticulo).";");
-        
+
        if ($date) return $date[0];
        else return FALSE;
-   } 
+   }
 
-   
+
 }
-
-
